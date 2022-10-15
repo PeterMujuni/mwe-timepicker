1373b98cf7cf25e9135d916086241166e759fc85
PeterMujuniMwebesa <kontakt@petermujuni.dk>
2022-10-16 00:10:13 +0200

    Adding webpack

--- public/main.js
+++
@@ -1,4 +0,0 @@
-import { activate } from "./activatePicker.js";
-import { setupRadioButtons } from "./setupRadioButtons.js";
-setupRadioButtons();
-activate();
--- public/numberToOption.js
+++
@@ -1,9 +0,0 @@
-/**
- *
- * @param {*} number
- * @returns
- */
-export const numberToOption = (number) => {
-    const padded = number.toString().padStart(2, "0");
-    return `<option value="${padded}">${padded}</option>`;
-};
